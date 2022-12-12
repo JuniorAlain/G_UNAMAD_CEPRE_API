e4efe5d4e4f574a5794bd3f23d4b4b158c2aa657
Junior Alain <jrancsar@gmail.com>
2022-12-12 14:57:41 -0500

    Preparacion de structura y dependencias

---
+++ G_UNAMAD_CEPRE_API/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace G_UNAMAD_CEPRE_API
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+        }
+    }
+}
--- G_UNAMAD_CEPRE_API/Program.cs
+++ G_UNAMAD_CEPRE_API/Program.cs
@@ -1,20 +1,22 @@
+using AutoMapper;
+using G_UNAMAD_CEPRE_API;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+var mapperConfig = new MapperConfiguration(m =>
+{
+    m.AddProfile(new MappingProfile());
+});
+
+IMapper mapper = mapperConfig.CreateMapper();
+
+builder.Services.AddSingleton(mapper);
+
+builder.Services.AddMvc();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
@@ -23,3 +25,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
