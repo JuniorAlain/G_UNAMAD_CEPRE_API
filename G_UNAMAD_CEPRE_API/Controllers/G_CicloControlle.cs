3d5a881f52a18bc51043a02627a8ba9ba6f7bdda
Junior Alain <jrancsar@gmail.com>
2022-12-12 16:57:34 -0500

    G_Ciclo: CRUD Data - Controller peticiones HTTP y alteracion ruta API

---
+++ G_UNAMAD_CEPRE_API/Controllers/G_CicloControlle.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using G_UNAMAD_CEPRE_API.Data;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G_UNAMAD_CEPRE_API.Controllers
+{
+    [ApiController]
+    [Route("api/Ciclo")]
+    public class G_CicloControlle : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public G_CicloControlle(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<G_CicloModel>>> G_CicloGetAll()
+        {
+            var funcion = new G_CicloData();
+            var lista = await funcion.G_CicloSelectAll();
+
+            return lista;
+        }
+        [HttpPost]
+        public async Task<ActionResult<ResponseSP>> G_CicloPost([FromBody] G_CicloModelDTOPost g_CicloModelDTOPost)
+        {
+            var funcion = new G_CicloData();
+            G_CicloModel g_CicloModel = _mapper.Map<G_CicloModel>(g_CicloModelDTOPost);
+            var respuestaSP = await funcion.G_CicloInsert(g_CicloModel);
+
+            return respuestaSP;
+        }
+        [HttpPut("{idCiclo}/{cVersion}")]
+        public async Task<ActionResult<ResponseSP>> G_CicloPut(string idCiclo, string cVersion, [FromBody] G_CicloModelDTOPut g_CicloModelDTOPut)
+        {
+            var funcion = new G_CicloData();
+            G_CicloModel g_CicloModel = _mapper.Map<G_CicloModel>(g_CicloModelDTOPut);
+            g_CicloModel.IdCiclo = idCiclo;
+            g_CicloModel.CVersion = cVersion;
+            var respuestaSP = await funcion.G_CicloUpdate(g_CicloModel);
+
+            return respuestaSP;
+        }
+        [HttpDelete("{idCiclo}/{cVersion}")]
+        public async Task<ActionResult<ResponseSP>> G_CicloDelete(string idCiclo, string cVersion)
+        {
+            var funcion = new G_CicloData();
+            G_CicloModel g_CicloModel = new G_CicloModel();
+            g_CicloModel.IdCiclo = idCiclo;
+            g_CicloModel.CVersion = cVersion;
+            var respuestaSP = await funcion.G_CicloDelete(g_CicloModel);
+
+            return respuestaSP;
+        }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Data/G_CicloData.cs
@@ -0,0 +1,135 @@
+﻿using G_UNAMAD_CEPRE_API.Connection;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace G_UNAMAD_CEPRE_API.Data
+{
+    public class G_CicloData
+    {
+        ConnectionBd cn = new ConnectionBd();
+        public async Task<List<G_CicloModel>> G_CicloSelectAll()
+        {
+            var lista = new List<G_CicloModel>();
+            //using para la conexion
+            using (var sql = new SqlConnection(cn.cadendaSQL()))
+            {
+                //using para ejecutar el procedimiento almacenado
+                using (var cmd = new SqlCommand("SP_G_CicloSelectAll", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    await sql.OpenAsync();
+                    //using para hacer el recorrido de datos
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await item.ReadAsync())
+                        {
+                            var g_CicloModel = new G_CicloModel();
+                            g_CicloModel.IdCiclo = (string)item["idCiclo"];
+                            g_CicloModel.NCiclo = (string)item["nCiclo"];
+                            g_CicloModel.Periodo = (string)item["periodo"];
+                            g_CicloModel.FInicio = (DateTime)item["fInicio"];
+                            g_CicloModel.FFin = (DateTime)item["fFin"];
+                            g_CicloModel.EProgreso = (int)item["eProgreso"];
+                            g_CicloModel.Activo = (int)item["activo"];
+                            g_CicloModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_CicloModel.CVersion = ConvertVersion((byte[])item["cVersion"]);
+                            lista.Add(g_CicloModel);
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+        public async Task<ResponseSP> G_CicloInsert(G_CicloModel g_CicloModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadendaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_CicloInsert", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idCiclo", g_CicloModel.IdCiclo);
+                    cmd.Parameters.AddWithValue("@nCiclo", g_CicloModel.NCiclo);
+                    cmd.Parameters.AddWithValue("@periodo", g_CicloModel.Periodo);
+                    cmd.Parameters.AddWithValue("@fInicio", g_CicloModel.FInicio);
+                    cmd.Parameters.AddWithValue("@fFin", g_CicloModel.FFin);
+                    cmd.Parameters.AddWithValue("@eProgreso", g_CicloModel.EProgreso);
+                    await sql.OpenAsync();
+                    //await cmd.ExecuteNonQueryAsync();
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+        public async Task<ResponseSP> G_CicloUpdate(G_CicloModel g_CicloModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadendaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_CicloUpdate", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idCiclo", g_CicloModel.IdCiclo);
+                    cmd.Parameters.AddWithValue("@nCiclo", g_CicloModel.NCiclo);
+                    cmd.Parameters.AddWithValue("@periodo", g_CicloModel.Periodo);
+                    cmd.Parameters.AddWithValue("@fInicio", g_CicloModel.FInicio);
+                    cmd.Parameters.AddWithValue("@fFin", g_CicloModel.FFin);
+                    cmd.Parameters.AddWithValue("@eProgreso", g_CicloModel.EProgreso);
+                    cmd.Parameters.AddWithValue("@activo", g_CicloModel.Activo);
+                    cmd.Parameters.AddWithValue("@cVersion", ConvertVersion(g_CicloModel.CVersion));
+                    await sql.OpenAsync();
+                    //await cmd.ExecuteNonQueryAsync();
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+        public async Task<ResponseSP> G_CicloDelete(G_CicloModel g_CicloModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadendaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_CicloDelete", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idCiclo", g_CicloModel.IdCiclo);
+                    cmd.Parameters.AddWithValue("@cVersion", ConvertVersion(g_CicloModel.CVersion));
+                    await sql.OpenAsync();
+                    //await cmd.ExecuteNonQueryAsync();
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+        private string ConvertVersion(byte[] cVersion)
+        {
+            return String.Concat("0x", BitConverter.ToString(cVersion).Replace("-", ""));
+        }
+        private byte[] ConvertVersion(string cVersion)
+        {
+            return Convert.FromHexString(cVersion.Replace("0x", ""));
+        }
+    }
+}
