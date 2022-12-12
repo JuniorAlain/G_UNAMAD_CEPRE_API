296796eea2f67e214bd179ed960723ea6d59b792
Junior Alain <jrancsar@gmail.com>
2022-12-12 16:44:03 -0500

    Creación clase ResponseSP

---
+++ G_UNAMAD_CEPRE_API/Models/ResponseSP.cs
@@ -0,0 +1,8 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class ResponseSP
+    {
+        public int TotalAffectedRecord { get; set; }
+        public string MessageException { get; set; }
+    }
+}
