5f98fd99d97dbf63d369e158b6c1d1cb81883775
Junior Alain <jrancsar@gmail.com>
2022-12-12 16:31:05 -0500

    G_Ciclo: Creacion de clase Modelo y DTO

---
+++ G_UNAMAD_CEPRE_API/Models/G_CicloModel.cs
@@ -0,0 +1,15 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_CicloModel
+    {
+        public string IdCiclo { get; set; }
+        public string NCiclo { set; get; }
+        public string Periodo { set; get; }
+        public DateTime FInicio { set; get; }
+        public DateTime FFin { set; get; }
+        public int EProgreso { set; get; }
+        public int Activo { set; get; }
+        public DateTime FRegistroM { set; get; }
+        public string CVersion { set; get; }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Models/G_CicloModelDTOPost.cs
@@ -0,0 +1,12 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_CicloModelDTOPost
+    {
+        public string idCiclo { get; set; }
+        public string nCiclo { set; get; }
+        public string periodo { set; get; }
+        public DateTime fInicio { set; get; }
+        public DateTime fFin { set; get; }
+        public int eProgreso { set; get; }
+    }
+}
