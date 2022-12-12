838b9c9cd4bb8618557b208302f8d8c3c575ac7c
Junior Alain <jrancsar@gmail.com>
2022-12-12 16:03:38 -0500

    Creacion y configuracion de conexion BD

---
+++ G_UNAMAD_CEPRE_API/Connection/ConnectionBd.cs
@@ -0,0 +1,18 @@
+﻿namespace G_UNAMAD_CEPRE_API.Connection
+{
+    public class ConnectionBd
+    {
+        private string connectionString = string.Empty;
+        public ConnectionBd()
+        {
+            var build = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            connectionString = build.GetSection("ConnectionStrings:Conexion").Value;
+        }
+
+        public string cadendaSQL()
+        {
+            return connectionString;
+        }
+    }
+}
