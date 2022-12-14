b1a15ae4b261adfcd9de7429e2e0bff479725d95
Junior Alain <jrancsar@gmail.com>
2022-12-14 14:32:38 -0500

    G_Ciclo: Unificación clase DTO y mapping

--- G_UNAMAD_CEPRE_API/MappingProfile.cs
+++ G_UNAMAD_CEPRE_API/MappingProfile.cs
@@ -7,16 +7,8 @@ namespace G_UNAMAD_CEPRE_API
     {
         public MappingProfile()
         {
-            CreateMap<G_CicloModelDTOPost, G_CicloModel>()
+            CreateMap<G_CicloModelDTO, G_CicloModel>()
                 .ForMember(d => d.IdCiclo, o => o.MapFrom(s => s.idCiclo))
-                .ForMember(d => d.NCiclo, o => o.MapFrom(s => s.nCiclo))
-                .ForMember(d => d.Periodo, o => o.MapFrom(s => s.periodo))
-                .ForMember(d => d.FInicio, o => o.MapFrom(s => s.fInicio))
-                .ForMember(d => d.FFin, o => o.MapFrom(s => s.fFin))
-                .ForMember(d => d.EProgreso, o => o.MapFrom(s => s.eProgreso))
-            ;
-
-            CreateMap<G_CicloModelDTOPut, G_CicloModel>()
                 .ForMember(d => d.NCiclo, o => o.MapFrom(s => s.nCiclo))
                 .ForMember(d => d.Periodo, o => o.MapFrom(s => s.periodo))
                 .ForMember(d => d.FInicio, o => o.MapFrom(s => s.fInicio))
