b0803da0dc410ce6889573ad74fe1c5ad63dcacb
Junior Alain <jrancsar@gmail.com>
2022-12-14 18:59:24 -0500

    G_Curso: Creacion de clase Modelo y DTO

---
+++ G_UNAMAD_CEPRE_API/Models/G_CursoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_CursoModel
+    {
+        public string IdCurso { get; set; }
+        public string NCurso { get; set; }
+        public int Activo { get; set; }
+        public DateTime FRegistroM { get; set; }
+        public string CVersion { get; set; }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Models/G_CursoModelDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_CursoModelDTO
+    {
+        public string idCurso { get; set; }
+        public string nCurso { get; set; }
+        public int activo { get; set; }                
+    }
+}
