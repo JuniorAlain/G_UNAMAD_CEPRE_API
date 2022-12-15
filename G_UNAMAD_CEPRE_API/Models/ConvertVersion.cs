07e33a58ffc6e2f1ec088f5d52efd5ee0f7b96e1
Junior Alain <jrancsar@gmail.com>
2022-12-14 22:18:07 -0500

    Clase ConvertVersion creada

---
+++ G_UNAMAD_CEPRE_API/Models/ConvertVersion.cs
@@ -0,0 +1,14 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class ConvertVersion
+    {
+        public string StringVersion(byte[] cVersion)
+        {
+            return String.Concat("0x", BitConverter.ToString(cVersion).Replace("-", ""));
+        }
+        public byte[] ByteVersion(string cVersion)
+        {
+            return Convert.FromHexString(cVersion.Replace("0x", ""));
+        }        
+    }
+}
