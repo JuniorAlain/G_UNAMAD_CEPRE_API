56eef81f219a75b0f09355980ebf605e456a59ea
Junior Alain <jrancsar@gmail.com>
2022-12-23 16:30:48 -0500

    G_Curso: Creación CRUD Data y Controller peticiones HTTP

---
+++ G_UNAMAD_CEPRE_API/Controllers/G_CursoController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using G_UNAMAD_CEPRE_API.Data;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G_UNAMAD_CEPRE_API.Controllers
+{
+    [ApiController]
+    [Route("api/Curso")]
+    public class G_CursoController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public G_CursoController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<G_CursoModel>>> G_CursoGetAll()
+        {
+            var funcion = new G_CursoData();
+            //var lista = await funcion.G_CursoSelectAll();
+
+            return await funcion.G_CursoSelectAll();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<G_CursoModel>> G_CursoGetId(string id)
+        {
+            var funcion = new G_CursoData();
+           //var g_CursoModel = await funcion.G_CursoSelectId(id);
+            
+            return await funcion.G_CursoSelectId(id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ResponseSP>> G_CursoPost([FromBody] G_CursoModelDTO g_CursoModelDTO)
+        {
+            var funcion = new G_CursoData();
+            G_CursoModel g_CursoModel = _mapper.Map<G_CursoModel>(g_CursoModelDTO);
+            //var responseSP = await funcion.G_CursoInsert(g_CursoModel);
+
+            return await funcion.G_CursoInsert(g_CursoModel);
+        }
+        [HttpPut("{id}/{cVersion}")]
+        public async Task<ActionResult<ResponseSP>> G_CursoPut(string id, string version, [FromBody] G_CursoModelDTO g_CursoModelDTO)
+        {
+            var funcion = new G_CursoData();
+            G_CursoModel g_CursoModel = _mapper.Map<G_CursoModel>(g_CursoModelDTO);
+            g_CursoModel.IdCurso = id;
+            g_CursoModel.CVersion = version;
+            //var responseSP = await funcion.G_CursoUpdate(g_CursoModel);
+
+            return await funcion.G_CursoUpdate(g_CursoModel);
+        }
+        [HttpDelete("{id}/{cVersion}")]
+        public async Task<ActionResult<ResponseSP>> G_CursoDelete(string id, string version)
+        {
+            var funcion = new G_CursoData();
+            //var responseSP = await funcion.G_CursoDelete(id, cVersion);
+            
+            return await funcion.G_CursoDelete(id, version);
+        }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Data/G_CursoData.cs
@@ -0,0 +1,140 @@
+﻿using G_UNAMAD_CEPRE_API.Connection;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace G_UNAMAD_CEPRE_API.Data
+{
+    public class G_CursoData : ConvertVersion
+    {
+        ConnectionBd cn = new ConnectionBd();        
+        public async Task<List<G_CursoModel>> G_CursoSelectAll()
+        {
+            var lista = new List<G_CursoModel>();
+            using (var sql = new SqlConnection(cn.cadendaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_CursoSelectAll", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await item.ReadAsync())
+                        {
+                            var g_CursoModel = new G_CursoModel();
+                            g_CursoModel.IdCurso = (string)item["idCurso"];
+                            g_CursoModel.NCurso = (string)item["nCurso"];
+                            g_CursoModel.Activo = (int)item["activo"];
+                            g_CursoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_CursoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                            lista.Add(g_CursoModel);
+                        }                          
+                    }
+                }                
+            }
+            return lista;
+        }
+        public async Task<G_CursoModel> G_CursoSelectId(string idCurso)
+        {
+            var g_CursoModel = new G_CursoModel();
+            using (var sql = new SqlConnection(cn.cadendaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_CursoSelectId", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idCurso", idCurso);
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {                   
+                            g_CursoModel.IdCurso = (string)item["idCurso"];
+                            g_CursoModel.NCurso = (string)item["nCurso"];
+                            g_CursoModel.Activo = (int)item["activo"];
+                            g_CursoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_CursoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                        }
+                    }
+                }                
+            }
+            return g_CursoModel;
+        }
+        public async Task<ResponseSP> G_CursoInsert(G_CursoModel g_CursoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadendaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_CursoInsert", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idCurso", g_CursoModel.IdCurso);
+                    cmd.Parameters.AddWithValue("@nCurso", g_CursoModel.NCurso);
+                    cmd.Parameters.AddWithValue("@activo", g_CursoModel.Activo);
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if(await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+        public async Task<ResponseSP> G_CursoUpdate(G_CursoModel g_CursoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadendaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_CursoUpdate", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idCurso", g_CursoModel.IdCurso);
+                    cmd.Parameters.AddWithValue("@nCurso", g_CursoModel.NCurso);
+                    cmd.Parameters.AddWithValue("@activo", g_CursoModel.Activo);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion(g_CursoModel.CVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+        public async Task<ResponseSP> G_CursoDelete(string idCurso, string cVersion)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadendaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_CursoDelete", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idCurso", idCurso);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion(cVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }                
+            }
+            return responseSP;
+        }
+    }
+}
