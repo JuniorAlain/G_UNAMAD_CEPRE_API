37a208416e55a707f667cc927361b689416ecc1f
Junior Alain <jrancsar@gmail.com>
2022-12-26 18:36:38 -0500

    G_Tema: Creación CRUD Data y Controller peticiones HTTP

---
+++ G_UNAMAD_CEPRE_API/Controllers/G_TemaController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using G_UNAMAD_CEPRE_API.Data;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G_UNAMAD_CEPRE_API.Controllers
+{
+    [ApiController]
+    [Route("api/Tema")]
+    public class G_TemaController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public G_TemaController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<G_TemaModel>>> G_TemaGetAll()
+        {
+            var funcion = new G_TemaData();
+
+            return await funcion.G_TemaSelectAll();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<G_TemaModel>> G_TemaGetId(string id)
+        {
+            var funcion = new G_TemaData();
+
+            return await funcion.G_TemaSelectId(id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ResponseSP>> G_TemaPost([FromBody]G_TemaModelDTO g_TemaModelDTO)
+        {
+            var funcion = new G_TemaData();
+            G_TemaModel g_TemaModel = _mapper.Map<G_TemaModel>(g_TemaModelDTO);
+
+            return await funcion.G_TemaInsert(g_TemaModel);
+        }
+        [HttpPut("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_TemaPut(string id, string version, [FromBody] G_TemaModelDTO g_TemaModelDTO)
+        {
+            var funcion = new G_TemaData();
+            G_TemaModel g_TemaModel = _mapper.Map<G_TemaModel>(g_TemaModelDTO);
+            g_TemaModel.IdTema = id;
+            g_TemaModel.CVersion = version;
+
+            return await funcion.G_TemaUpdate(g_TemaModel);
+        }
+        [HttpDelete("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_TemaDelete(string id, string version)
+        {
+            var funcion = new G_TemaData();
+
+            return await funcion.G_TemaDelete(id, version);
+        }
+    }    
+}
---
+++ G_UNAMAD_CEPRE_API/Data/G_TemaData.cs
@@ -0,0 +1,139 @@
+﻿using G_UNAMAD_CEPRE_API.Connection;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace G_UNAMAD_CEPRE_API.Data
+{
+    public class G_TemaData : ConvertVersion
+    {
+        ConnectionBd cn = new ConnectionBd();
+        public async Task<List<G_TemaModel>> G_TemaSelectAll()
+        {
+            var lista = new List<G_TemaModel>();
+            using(var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using(var cmd = new SqlCommand("SP_G_TemaSelectAll", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    await sql.OpenAsync();
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        while(await item.ReadAsync())
+                        {
+                            var g_TemaModel = new G_TemaModel();
+                            g_TemaModel.IdTema = (string)item["idTema"];
+                            g_TemaModel.NTema = (string)item["nTema"];
+                            g_TemaModel.Activo = (int)item["activo"];
+                            g_TemaModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_TemaModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                            lista.Add(g_TemaModel);
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+        public async Task<G_TemaModel> G_TemaSelectId(string idTema)
+        {
+            var g_TemaModel = new G_TemaModel();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_TemaSelectId", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idTema", idTema);
+                    await sql.OpenAsync();
+
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            g_TemaModel.IdTema = (string)item["idTema"];
+                            g_TemaModel.NTema = (string)item["nTema"];
+                            g_TemaModel.Activo = (int)item["activo"];
+                            g_TemaModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_TemaModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                        }                        
+                    }
+                }
+            }
+            return g_TemaModel;
+        }
+        public async Task<ResponseSP> G_TemaInsert(G_TemaModel g_TemaModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_TemaInsert", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idTema", g_TemaModel.IdTema);
+                    cmd.Parameters.AddWithValue("@nTema", g_TemaModel.NTema);
+                    cmd.Parameters.AddWithValue("@activo", g_TemaModel.Activo);
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+        public async Task<ResponseSP> G_TemaUpdate(G_TemaModel g_TemaModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_TemaUpdate", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idTema", g_TemaModel.IdTema);
+                    cmd.Parameters.AddWithValue("@nTema", g_TemaModel.NTema);
+                    cmd.Parameters.AddWithValue("@activo", g_TemaModel.Activo);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)g_TemaModel.CVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }            
+            return responseSP;
+        }
+        public async Task<ResponseSP>G_TemaDelete(string idTema, string cVersion)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_TemaDelete", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idTema", idTema);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)cVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+    }    
+}
