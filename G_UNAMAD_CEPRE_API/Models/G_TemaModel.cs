d2bbe93b26e128e25601568f3266b66713d96fcf
Junior Alain <jrancsar@gmail.com>
2022-12-26 18:35:15 -0500

    G_Tema: Creacion de clase Modelo y DTO

---
+++ G_UNAMAD_CEPRE_API/Models/G_TemaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_TemaModel
+    {
+        public string IdTema { get; set; }
+        public string NTema { get; set; }
+        public int Activo { get; set; }
+        public DateTime FRegistroM { get; set; }
+        public string CVersion { get; set; }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Models/G_TemaModelDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_TemaModelDTO
+    {
+        public string idTema { get; set; }
+        public string nTema { get; set; }
+        public int activo { get; set; }
+    }
+}
