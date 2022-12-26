a2ef0cc3cade3b309173c8b8ddf6dc8a9e6f6468
Junior Alain <jrancsar@gmail.com>
2022-12-26 10:58:00 -0500

    G_Carrera: Creacion de clase Modelo y DTO

---
+++ G_UNAMAD_CEPRE_API/Models/G_CarreraModel.cs
@@ -0,0 +1,11 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_CarreraModel
+    {
+        public string IdCarrera { get; set; }
+        public string NCarrera { get; set; }
+        public int Activo { get; set; }
+        public DateTime FRegistroM { get; set; }
+        public string CVersion { get; set; }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Models/G_CarreraModelDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_CarreraModelDTO
+    {
+        public string idCarrera { get; set; }
+        public string nCarrera { get; set; }
+        public int activo { get; set; }
+    }
+}
