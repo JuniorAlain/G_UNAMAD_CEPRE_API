e02bc0a70c991558efae7187f3be787e69bfcc7d
Junior Alain <jrancsar@gmail.com>
2022-12-26 18:49:09 -0500

    G_Turno: Creación CRUD Data y Controller peticiones HTTP

---
+++ G_UNAMAD_CEPRE_API/Controllers/G_TurnoController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using G_UNAMAD_CEPRE_API.Data;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G_UNAMAD_CEPRE_API.Controllers
+{
+    [ApiController]
+    [Route("api/Turno")]
+    public class G_TurnoController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public G_TurnoController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<G_TurnoModel>>> G_TurnoGetAll()
+        {
+            var funcion = new G_TurnoData();
+
+            return await funcion.G_TurnoSelectAll();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<G_TurnoModel>> G_TurnoGetId(string id)
+        {
+            var funcion = new G_TurnoData();
+
+            return await funcion.G_TurnoSelectId(id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ResponseSP>> G_TurnoPost([FromBody]G_TurnoModelDTO g_TurnoModelDTO)
+        {
+            var funcion = new G_TurnoData();
+            G_TurnoModel g_TurnoModel = _mapper.Map<G_TurnoModel>(g_TurnoModelDTO);
+
+            return await funcion.G_TurnoInsert(g_TurnoModel);
+        }
+        [HttpPut("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_TurnoPut(string id, string version, [FromBody] G_TurnoModelDTO g_TurnoModelDTO)
+        {
+            var funcion = new G_TurnoData();
+            G_TurnoModel g_TurnoModel = _mapper.Map<G_TurnoModel>(g_TurnoModelDTO);
+            g_TurnoModel.IdTurno = id;
+            g_TurnoModel.CVersion = version;
+
+            return await funcion.G_TurnoUpdate(g_TurnoModel);
+        }
+        [HttpDelete("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_TurnoDelete(string id, string version)
+        {
+            var funcion = new G_TurnoData();
+
+            return await funcion.G_TurnoDelete(id, version);
+        }
+    }    
+}
---
+++ G_UNAMAD_CEPRE_API/Data/G_TurnoData.cs
@@ -0,0 +1,139 @@
+﻿using G_UNAMAD_CEPRE_API.Connection;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace G_UNAMAD_CEPRE_API.Data
+{
+    public class G_TurnoData : ConvertVersion
+    {
+        ConnectionBd cn = new ConnectionBd();
+        public async Task<List<G_TurnoModel>> G_TurnoSelectAll()
+        {
+            var lista = new List<G_TurnoModel>();
+            using(var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using(var cmd = new SqlCommand("SP_G_TurnoSelectAll", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    await sql.OpenAsync();
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        while(await item.ReadAsync())
+                        {
+                            var g_TurnoModel = new G_TurnoModel();
+                            g_TurnoModel.IdTurno = (string)item["idTurno"];
+                            g_TurnoModel.NTurno = (string)item["nTurno"];
+                            g_TurnoModel.Activo = (int)item["activo"];
+                            g_TurnoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_TurnoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                            lista.Add(g_TurnoModel);
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+        public async Task<G_TurnoModel> G_TurnoSelectId(string idTurno)
+        {
+            var g_TurnoModel = new G_TurnoModel();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_TurnoSelectId", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idTurno", idTurno);
+                    await sql.OpenAsync();
+
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            g_TurnoModel.IdTurno = (string)item["idTurno"];
+                            g_TurnoModel.NTurno = (string)item["nTurno"];
+                            g_TurnoModel.Activo = (int)item["activo"];
+                            g_TurnoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_TurnoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                        }                        
+                    }
+                }
+            }
+            return g_TurnoModel;
+        }
+        public async Task<ResponseSP> G_TurnoInsert(G_TurnoModel g_TurnoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_TurnoInsert", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idTurno", g_TurnoModel.IdTurno);
+                    cmd.Parameters.AddWithValue("@nTurno", g_TurnoModel.NTurno);
+                    cmd.Parameters.AddWithValue("@activo", g_TurnoModel.Activo);
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+        public async Task<ResponseSP> G_TurnoUpdate(G_TurnoModel g_TurnoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_TurnoUpdate", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idTurno", g_TurnoModel.IdTurno);
+                    cmd.Parameters.AddWithValue("@nTurno", g_TurnoModel.NTurno);
+                    cmd.Parameters.AddWithValue("@activo", g_TurnoModel.Activo);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)g_TurnoModel.CVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }            
+            return responseSP;
+        }
+        public async Task<ResponseSP>G_TurnoDelete(string idTurno, string cVersion)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_TurnoDelete", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idTurno", idTurno);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)cVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+    }    
+}
