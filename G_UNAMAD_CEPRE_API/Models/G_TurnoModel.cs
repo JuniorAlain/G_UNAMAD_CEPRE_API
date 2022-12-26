3fadb5c834e9cf5acbb84839234929619c5505d0
Junior Alain <jrancsar@gmail.com>
2022-12-26 18:47:54 -0500

    G_Turno: Creacion de clase Modelo y DTO

---
+++ G_UNAMAD_CEPRE_API/Models/G_TurnoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_TurnoModel
+    {
+        public string IdTurno { get; set; }
+        public string NTurno { get; set; }
+        public int Activo { get; set; }
+        public DateTime FRegistroM { get; set; }
+        public string CVersion { get; set; }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Models/G_TurnoModelDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_TurnoModelDTO
+    {
+        public string idTurno { get; set; }
+        public string nTurno { get; set; }
+        public int activo { get; set; }
+    }
+}
