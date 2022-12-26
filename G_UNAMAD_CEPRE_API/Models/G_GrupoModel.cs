b2af28f972df4b0083c13d40d175b7b7e086630a
Junior Alain <jrancsar@gmail.com>
2022-12-26 18:16:38 -0500

    G_Grupo: Creacion de clase Modelo y DTO

---
+++ G_UNAMAD_CEPRE_API/Models/G_GrupoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_GrupoModel
+    {
+        public string IdGrupo { get; set; }
+        public string NGrupo { get; set; }
+        public int Activo { get; set; }
+        public DateTime FRegistroM { get; set; }
+        public string CVersion { get; set; }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Models/G_GrupoModelDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_GrupoModelDTO
+    {
+        public string idGrupo { get; set; }
+        public string nGrupo { get; set; }
+        public int activo { get; set; }
+    }
+}
