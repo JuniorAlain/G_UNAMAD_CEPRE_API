5505c14fc64083806837596755ca93b955dffc25
Junior Alain <jrancsar@gmail.com>
2022-12-26 17:52:16 -0500

    G_Carrera: Creación CRUD Data y Controller peticiones HTTP

---
+++ G_UNAMAD_CEPRE_API/Controllers/G_CarreraController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using G_UNAMAD_CEPRE_API.Data;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G_UNAMAD_CEPRE_API.Controllers
+{
+    [ApiController]
+    [Route("api/Carrera")]
+    public class G_CarreraController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public G_CarreraController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<G_CarreraModel>>> G_CarreraGetAll()
+        {
+            var funcion = new G_CarreraData();
+
+            return await funcion.G_CarreraSelectAll();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<G_CarreraModel>> G_CarreraGetId(string id)
+        {
+            var funcion = new G_CarreraData();
+
+            return await funcion.G_CarreraSelectId(id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ResponseSP>> G_CarreraPost([FromBody]G_CarreraModelDTO g_CarreraModelDTO)
+        {
+            var funcion = new G_CarreraData();
+            G_CarreraModel g_CarreraModel = _mapper.Map<G_CarreraModel>(g_CarreraModelDTO);
+
+            return await funcion.G_CarreraInsert(g_CarreraModel);
+        }
+        [HttpPut("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_CarreraPut(string id, string version, [FromBody] G_CarreraModelDTO g_CarreraModelDTO)
+        {
+            var funcion = new G_CarreraData();
+            G_CarreraModel g_CarreraModel = _mapper.Map<G_CarreraModel>(g_CarreraModelDTO);
+            g_CarreraModel.IdCarrera = id;
+            g_CarreraModel.CVersion = version;
+
+            return await funcion.G_CarreraUpdate(g_CarreraModel);
+        }
+        [HttpDelete("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_CarreraDelete(string id, string version)
+        {
+            var funcion = new G_CarreraData();
+
+            return await funcion.G_CarreraDelete(id, version);
+        }
+    }    
+}
---
+++ G_UNAMAD_CEPRE_API/Data/G_CarreraData.cs
@@ -0,0 +1,139 @@
+﻿using G_UNAMAD_CEPRE_API.Connection;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace G_UNAMAD_CEPRE_API.Data
+{
+    public class G_CarreraData : ConvertVersion
+    {
+        ConnectionBd cn = new ConnectionBd();
+        public async Task<List<G_CarreraModel>> G_CarreraSelectAll()
+        {
+            var lista = new List<G_CarreraModel>();
+            using(var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using(var cmd = new SqlCommand("SP_G_CarreraSelectAll", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    await sql.OpenAsync();
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        while(await item.ReadAsync())
+                        {
+                            var g_CarreraModel = new G_CarreraModel();
+                            g_CarreraModel.IdCarrera = (string)item["idCarrera"];
+                            g_CarreraModel.NCarrera = (string)item["nCarrera"];
+                            g_CarreraModel.Activo = (int)item["activo"];
+                            g_CarreraModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_CarreraModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                            lista.Add(g_CarreraModel);
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+        public async Task<G_CarreraModel> G_CarreraSelectId(string idCarrera)
+        {
+            var g_CarreraModel = new G_CarreraModel();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_CarreraSelectId", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idCarrera", idCarrera);
+                    await sql.OpenAsync();
+
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            g_CarreraModel.IdCarrera = (string)item["idCarrera"];
+                            g_CarreraModel.NCarrera = (string)item["nCarrera"];
+                            g_CarreraModel.Activo = (int)item["activo"];
+                            g_CarreraModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_CarreraModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                        }                        
+                    }
+                }
+            }
+            return g_CarreraModel;
+        }
+        public async Task<ResponseSP> G_CarreraInsert(G_CarreraModel g_CarreraModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_CarreraInsert", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idCarrera", g_CarreraModel.IdCarrera);
+                    cmd.Parameters.AddWithValue("@nCarrera", g_CarreraModel.NCarrera);
+                    cmd.Parameters.AddWithValue("@activo", g_CarreraModel.Activo);
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+        public async Task<ResponseSP> G_CarreraUpdate(G_CarreraModel g_CarreraModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_CarreraUpdate", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idCarrera", g_CarreraModel.IdCarrera);
+                    cmd.Parameters.AddWithValue("@nCarrera", g_CarreraModel.NCarrera);
+                    cmd.Parameters.AddWithValue("@activo", g_CarreraModel.Activo);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)g_CarreraModel.CVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }            
+            return responseSP;
+        }
+        public async Task<ResponseSP>G_CarreraDelete(string idCarrera, string cVersion)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_CarreraDelete", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idCarrera", idCarrera);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)cVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+    }    
+}
