9bfb355ead212d3c230b9539b4a3e5a2c821f333
Junior Alain <jrancsar@gmail.com>
2022-12-26 18:19:50 -0500

    G_Grupo: Creación CRUD Data y Controller peticiones HTTP

---
+++ G_UNAMAD_CEPRE_API/Controllers/G_GrupoController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using G_UNAMAD_CEPRE_API.Data;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G_UNAMAD_CEPRE_API.Controllers
+{
+    [ApiController]
+    [Route("api/Grupo")]
+    public class G_GrupoController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public G_GrupoController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<G_GrupoModel>>> G_GrupoGetAll()
+        {
+            var funcion = new G_GrupoData();
+
+            return await funcion.G_GrupoSelectAll();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<G_GrupoModel>> G_GrupoGetId(string id)
+        {
+            var funcion = new G_GrupoData();
+
+            return await funcion.G_GrupoSelectId(id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ResponseSP>> G_GrupoPost([FromBody]G_GrupoModelDTO g_GrupoModelDTO)
+        {
+            var funcion = new G_GrupoData();
+            G_GrupoModel g_GrupoModel = _mapper.Map<G_GrupoModel>(g_GrupoModelDTO);
+
+            return await funcion.G_GrupoInsert(g_GrupoModel);
+        }
+        [HttpPut("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_GrupoPut(string id, string version, [FromBody] G_GrupoModelDTO g_GrupoModelDTO)
+        {
+            var funcion = new G_GrupoData();
+            G_GrupoModel g_GrupoModel = _mapper.Map<G_GrupoModel>(g_GrupoModelDTO);
+            g_GrupoModel.IdGrupo = id;
+            g_GrupoModel.CVersion = version;
+
+            return await funcion.G_GrupoUpdate(g_GrupoModel);
+        }
+        [HttpDelete("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_GrupoDelete(string id, string version)
+        {
+            var funcion = new G_GrupoData();
+
+            return await funcion.G_GrupoDelete(id, version);
+        }
+    }    
+}
---
+++ G_UNAMAD_CEPRE_API/Data/G_GrupoData.cs
@@ -0,0 +1,139 @@
+﻿using G_UNAMAD_CEPRE_API.Connection;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace G_UNAMAD_CEPRE_API.Data
+{
+    public class G_GrupoData : ConvertVersion
+    {
+        ConnectionBd cn = new ConnectionBd();
+        public async Task<List<G_GrupoModel>> G_GrupoSelectAll()
+        {
+            var lista = new List<G_GrupoModel>();
+            using(var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using(var cmd = new SqlCommand("SP_G_GrupoSelectAll", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    await sql.OpenAsync();
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        while(await item.ReadAsync())
+                        {
+                            var g_GrupoModel = new G_GrupoModel();
+                            g_GrupoModel.IdGrupo = (string)item["idGrupo"];
+                            g_GrupoModel.NGrupo = (string)item["nGrupo"];
+                            g_GrupoModel.Activo = (int)item["activo"];
+                            g_GrupoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_GrupoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                            lista.Add(g_GrupoModel);
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+        public async Task<G_GrupoModel> G_GrupoSelectId(string idGrupo)
+        {
+            var g_GrupoModel = new G_GrupoModel();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_GrupoSelectId", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idGrupo", idGrupo);
+                    await sql.OpenAsync();
+
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            g_GrupoModel.IdGrupo = (string)item["idGrupo"];
+                            g_GrupoModel.NGrupo = (string)item["nGrupo"];
+                            g_GrupoModel.Activo = (int)item["activo"];
+                            g_GrupoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_GrupoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                        }                        
+                    }
+                }
+            }
+            return g_GrupoModel;
+        }
+        public async Task<ResponseSP> G_GrupoInsert(G_GrupoModel g_GrupoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_GrupoInsert", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idGrupo", g_GrupoModel.IdGrupo);
+                    cmd.Parameters.AddWithValue("@nGrupo", g_GrupoModel.NGrupo);
+                    cmd.Parameters.AddWithValue("@activo", g_GrupoModel.Activo);
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+        public async Task<ResponseSP> G_GrupoUpdate(G_GrupoModel g_GrupoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_GrupoUpdate", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idGrupo", g_GrupoModel.IdGrupo);
+                    cmd.Parameters.AddWithValue("@nGrupo", g_GrupoModel.NGrupo);
+                    cmd.Parameters.AddWithValue("@activo", g_GrupoModel.Activo);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)g_GrupoModel.CVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }            
+            return responseSP;
+        }
+        public async Task<ResponseSP>G_GrupoDelete(string idGrupo, string cVersion)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_GrupoDelete", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idGrupo", idGrupo);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)cVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+    }    
+}
