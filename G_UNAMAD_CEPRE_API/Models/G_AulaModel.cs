902d1a1e8191a552f1a5dc5ef7fcc7c47e348a0e
Junior Alain <jrancsar@gmail.com>
2022-12-26 18:59:06 -0500

    G_Aula: Creacion de clase Modelo y DTO

---
+++ G_UNAMAD_CEPRE_API/Models/G_AulaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_AulaModel
+    {
+        public string IdAula { get; set; }
+        public string NAula { get; set; }
+        public int Activo { get; set; }
+        public DateTime FRegistroM { get; set; }
+        public string CVersion { get; set; }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Models/G_AulaModelDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_AulaModelDTO
+    {
+        public string idAula { get; set; }
+        public string nAula { get; set; }
+        public int activo { get; set; }
+    }
+}
