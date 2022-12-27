ef2cb6360429ce958dd215a39051af1a27c592eb
Junior Alain <jrancsar@gmail.com>
2022-12-26 20:40:07 -0500

    G_GradoAcademico: Creacion de clase Modelo y DTO

---
+++ G_UNAMAD_CEPRE_API/Models/G_GradoAcademicoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_GradoAcademicoModel
+    {
+        public string IdGradoAcademico { get; set; }
+        public string NGradoAcademico { get; set; }
+        public int Activo { get; set; }
+        public DateTime FRegistroM { get; set; }
+        public string CVersion { get; set; }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Models/G_GradoAcademicoModelDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_GradoAcademicoModelDTO
+    {
+        public string idGradoAcademico { get; set; }
+        public string nGradoAcademico { get; set; }
+        public int activo { get; set; }
+    }
+}
