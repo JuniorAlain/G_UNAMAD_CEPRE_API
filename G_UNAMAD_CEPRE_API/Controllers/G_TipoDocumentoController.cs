4655dd1b2ddecc75472b4482db615fddf77a5b5a
Junior Alain <jrancsar@gmail.com>
2022-12-26 19:38:46 -0500

    G_TipoDocumento: Creación CRUD Data y Controller peticiones HTTP

---
+++ G_UNAMAD_CEPRE_API/Controllers/G_TipoDocumentoController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using G_UNAMAD_CEPRE_API.Data;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G_UNAMAD_CEPRE_API.Controllers
+{
+    [ApiController]
+    [Route("api/TipoDocumento")]
+    public class G_TipoDocumentoController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public G_TipoDocumentoController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<G_TipoDocumentoModel>>> G_TipoDocumentoGetAll()
+        {
+            var funcion = new G_TipoDocumentoData();
+
+            return await funcion.G_TipoDocumentoSelectAll();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<G_TipoDocumentoModel>> G_TipoDocumentoGetId(string id)
+        {
+            var funcion = new G_TipoDocumentoData();
+
+            return await funcion.G_TipoDocumentoSelectId(id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ResponseSP>> G_TipoDocumentoPost([FromBody]G_TipoDocumentoModelDTO g_TipoDocumentoModelDTO)
+        {
+            var funcion = new G_TipoDocumentoData();
+            G_TipoDocumentoModel g_TipoDocumentoModel = _mapper.Map<G_TipoDocumentoModel>(g_TipoDocumentoModelDTO);
+
+            return await funcion.G_TipoDocumentoInsert(g_TipoDocumentoModel);
+        }
+        [HttpPut("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_TipoDocumentoPut(string id, string version, [FromBody] G_TipoDocumentoModelDTO g_TipoDocumentoModelDTO)
+        {
+            var funcion = new G_TipoDocumentoData();
+            G_TipoDocumentoModel g_TipoDocumentoModel = _mapper.Map<G_TipoDocumentoModel>(g_TipoDocumentoModelDTO);
+            g_TipoDocumentoModel.IdTipoDocumento = id;
+            g_TipoDocumentoModel.CVersion = version;
+
+            return await funcion.G_TipoDocumentoUpdate(g_TipoDocumentoModel);
+        }
+        [HttpDelete("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_TipoDocumentoDelete(string id, string version)
+        {
+            var funcion = new G_TipoDocumentoData();
+
+            return await funcion.G_TipoDocumentoDelete(id, version);
+        }
+    }    
+}
---
+++ G_UNAMAD_CEPRE_API/Data/G_TipoDocumentoData.cs
@@ -0,0 +1,139 @@
+﻿using G_UNAMAD_CEPRE_API.Connection;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace G_UNAMAD_CEPRE_API.Data
+{
+    public class G_TipoDocumentoData : ConvertVersion
+    {
+        ConnectionBd cn = new ConnectionBd();
+        public async Task<List<G_TipoDocumentoModel>> G_TipoDocumentoSelectAll()
+        {
+            var lista = new List<G_TipoDocumentoModel>();
+            using(var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using(var cmd = new SqlCommand("SP_G_TipoDocumentoSelectAll", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    await sql.OpenAsync();
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        while(await item.ReadAsync())
+                        {
+                            var g_TipoDocumentoModel = new G_TipoDocumentoModel();
+                            g_TipoDocumentoModel.IdTipoDocumento = (string)item["idTipoDocumento"];
+                            g_TipoDocumentoModel.NTipoDocumento = (string)item["nTipoDocumento"];
+                            g_TipoDocumentoModel.Activo = (int)item["activo"];
+                            g_TipoDocumentoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_TipoDocumentoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                            lista.Add(g_TipoDocumentoModel);
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+        public async Task<G_TipoDocumentoModel> G_TipoDocumentoSelectId(string idTipoDocumento)
+        {
+            var g_TipoDocumentoModel = new G_TipoDocumentoModel();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_TipoDocumentoSelectId", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idTipoDocumento", idTipoDocumento);
+                    await sql.OpenAsync();
+
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            g_TipoDocumentoModel.IdTipoDocumento = (string)item["idTipoDocumento"];
+                            g_TipoDocumentoModel.NTipoDocumento = (string)item["nTipoDocumento"];
+                            g_TipoDocumentoModel.Activo = (int)item["activo"];
+                            g_TipoDocumentoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_TipoDocumentoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                        }                        
+                    }
+                }
+            }
+            return g_TipoDocumentoModel;
+        }
+        public async Task<ResponseSP> G_TipoDocumentoInsert(G_TipoDocumentoModel g_TipoDocumentoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_TipoDocumentoInsert", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idTipoDocumento", g_TipoDocumentoModel.IdTipoDocumento);
+                    cmd.Parameters.AddWithValue("@nTipoDocumento", g_TipoDocumentoModel.NTipoDocumento);
+                    cmd.Parameters.AddWithValue("@activo", g_TipoDocumentoModel.Activo);
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+        public async Task<ResponseSP> G_TipoDocumentoUpdate(G_TipoDocumentoModel g_TipoDocumentoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_TipoDocumentoUpdate", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idTipoDocumento", g_TipoDocumentoModel.IdTipoDocumento);
+                    cmd.Parameters.AddWithValue("@nTipoDocumento", g_TipoDocumentoModel.NTipoDocumento);
+                    cmd.Parameters.AddWithValue("@activo", g_TipoDocumentoModel.Activo);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)g_TipoDocumentoModel.CVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }            
+            return responseSP;
+        }
+        public async Task<ResponseSP>G_TipoDocumentoDelete(string idTipoDocumento, string cVersion)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_TipoDocumentoDelete", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idTipoDocumento", idTipoDocumento);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)cVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+    }    
+}
