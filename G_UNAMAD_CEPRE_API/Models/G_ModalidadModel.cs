4c222469cded562e0da99f126a275f4cb4f407ef
Junior Alain <jrancsar@gmail.com>
2022-12-26 19:24:44 -0500

    G_Modalidad: Creacion de clase Modelo y DTO

---
+++ G_UNAMAD_CEPRE_API/Models/G_ModalidadModel.cs
@@ -0,0 +1,11 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_ModalidadModel
+    {
+        public string IdModalidad { get; set; }
+        public string NModalidad { get; set; }
+        public int Activo { get; set; }
+        public DateTime FRegistroM { get; set; }
+        public string CVersion { get; set; }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Models/G_ModalidadModelDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_ModalidadModelDTO
+    {
+        public string idModalidad { get; set; }
+        public string nModalidad { get; set; }
+        public int activo { get; set; }
+    }
+}
