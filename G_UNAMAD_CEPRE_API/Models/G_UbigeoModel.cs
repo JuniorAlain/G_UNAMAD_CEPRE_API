6e857bbec2b5b3b2cc23246dcf04f423979d97f2
Junior Alain <jrancsar@gmail.com>
2022-12-26 21:39:08 -0500

    G_Ubigeo: Creacion de clase Modelo y DTO

---
+++ G_UNAMAD_CEPRE_API/Models/G_UbigeoModel.cs
@@ -0,0 +1,16 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_UbigeoModel
+    {
+        public string IdUbigeo { get; set; }
+        public string CDepartamento { get; set; }
+        public string NDepartamento { get; set; }
+        public string CProvincia { get; set; }
+        public string NProvincia { get; set; }
+        public string CDistrito { get; set; }
+        public string NDistrito { get; set; }
+        public int Activo { get; set; }
+        public DateTime FRegistroM { get; set; }
+        public string CVersion { get; set; }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Models/G_UbigeoModelDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_UbigeoModelDTO
+    {
+        public string idUbigeo { get; set; }
+        public string cDepartamento { get; set; }
+        public string nDepartamento { get; set; }
+        public string cProvincia { get; set; }
+        public string nProvincia { get; set; }
+        public string cDistrito { get; set; }
+        public string nDistrito { get; set; }
+        public int activo { get; set; }        
+    }
+}
