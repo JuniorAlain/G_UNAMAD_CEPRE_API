e6fcf0b8afa34f4aedfdba04b8dbef98fd0751f9
Junior Alain <jrancsar@gmail.com>
2022-12-26 21:22:04 -0500

    G_DocumentoGrado: Creación CRUD Data y Controller peticiones HTTP

---
+++ G_UNAMAD_CEPRE_API/Controllers/G_DocumentoGradoController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using G_UNAMAD_CEPRE_API.Data;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G_UNAMAD_CEPRE_API.Controllers
+{
+    [ApiController]
+    [Route("api/DocumentoGrado")]
+    public class G_DocumentoGradoController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public G_DocumentoGradoController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<G_DocumentoGradoModel>>> G_DocumentoGradoGetAll()
+        {
+            var funcion = new G_DocumentoGradoData();
+
+            return await funcion.G_DocumentoGradoSelectAll();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<G_DocumentoGradoModel>> G_DocumentoGradoGetId(string id)
+        {
+            var funcion = new G_DocumentoGradoData();
+
+            return await funcion.G_DocumentoGradoSelectId(id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ResponseSP>> G_DocumentoGradoPost([FromBody]G_DocumentoGradoModelDTO g_DocumentoGradoModelDTO)
+        {
+            var funcion = new G_DocumentoGradoData();
+            G_DocumentoGradoModel g_DocumentoGradoModel = _mapper.Map<G_DocumentoGradoModel>(g_DocumentoGradoModelDTO);
+
+            return await funcion.G_DocumentoGradoInsert(g_DocumentoGradoModel);
+        }
+        [HttpPut("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_DocumentoGradoPut(string id, string version, [FromBody] G_DocumentoGradoModelDTO g_DocumentoGradoModelDTO)
+        {
+            var funcion = new G_DocumentoGradoData();
+            G_DocumentoGradoModel g_DocumentoGradoModel = _mapper.Map<G_DocumentoGradoModel>(g_DocumentoGradoModelDTO);
+            g_DocumentoGradoModel.IdDocumentoGrado = id;
+            g_DocumentoGradoModel.CVersion = version;
+
+            return await funcion.G_DocumentoGradoUpdate(g_DocumentoGradoModel);
+        }
+        [HttpDelete("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_DocumentoGradoDelete(string id, string version)
+        {
+            var funcion = new G_DocumentoGradoData();
+
+            return await funcion.G_DocumentoGradoDelete(id, version);
+        }
+    }    
+}
---
+++ G_UNAMAD_CEPRE_API/Data/G_DocumentoGradoData.cs
@@ -0,0 +1,139 @@
+﻿using G_UNAMAD_CEPRE_API.Connection;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace G_UNAMAD_CEPRE_API.Data
+{
+    public class G_DocumentoGradoData : ConvertVersion
+    {
+        ConnectionBd cn = new ConnectionBd();
+        public async Task<List<G_DocumentoGradoModel>> G_DocumentoGradoSelectAll()
+        {
+            var lista = new List<G_DocumentoGradoModel>();
+            using(var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using(var cmd = new SqlCommand("SP_G_DocumentoGradoSelectAll", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    await sql.OpenAsync();
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        while(await item.ReadAsync())
+                        {
+                            var g_DocumentoGradoModel = new G_DocumentoGradoModel();
+                            g_DocumentoGradoModel.IdDocumentoGrado = (string)item["idDocumentoGrado"];
+                            g_DocumentoGradoModel.NDocumentoGrado = (string)item["nDocumentoGrado"];
+                            g_DocumentoGradoModel.Activo = (int)item["activo"];
+                            g_DocumentoGradoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_DocumentoGradoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                            lista.Add(g_DocumentoGradoModel);
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+        public async Task<G_DocumentoGradoModel> G_DocumentoGradoSelectId(string idDocumentoGrado)
+        {
+            var g_DocumentoGradoModel = new G_DocumentoGradoModel();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_DocumentoGradoSelectId", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idDocumentoGrado", idDocumentoGrado);
+                    await sql.OpenAsync();
+
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            g_DocumentoGradoModel.IdDocumentoGrado = (string)item["idDocumentoGrado"];
+                            g_DocumentoGradoModel.NDocumentoGrado = (string)item["nDocumentoGrado"];
+                            g_DocumentoGradoModel.Activo = (int)item["activo"];
+                            g_DocumentoGradoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_DocumentoGradoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                        }                        
+                    }
+                }
+            }
+            return g_DocumentoGradoModel;
+        }
+        public async Task<ResponseSP> G_DocumentoGradoInsert(G_DocumentoGradoModel g_DocumentoGradoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_DocumentoGradoInsert", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idDocumentoGrado", g_DocumentoGradoModel.IdDocumentoGrado);
+                    cmd.Parameters.AddWithValue("@nDocumentoGrado", g_DocumentoGradoModel.NDocumentoGrado);
+                    cmd.Parameters.AddWithValue("@activo", g_DocumentoGradoModel.Activo);
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+        public async Task<ResponseSP> G_DocumentoGradoUpdate(G_DocumentoGradoModel g_DocumentoGradoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_DocumentoGradoUpdate", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idDocumentoGrado", g_DocumentoGradoModel.IdDocumentoGrado);
+                    cmd.Parameters.AddWithValue("@nDocumentoGrado", g_DocumentoGradoModel.NDocumentoGrado);
+                    cmd.Parameters.AddWithValue("@activo", g_DocumentoGradoModel.Activo);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)g_DocumentoGradoModel.CVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }            
+            return responseSP;
+        }
+        public async Task<ResponseSP>G_DocumentoGradoDelete(string idDocumentoGrado, string cVersion)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_DocumentoGradoDelete", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idDocumentoGrado", idDocumentoGrado);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)cVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+    }    
+}
