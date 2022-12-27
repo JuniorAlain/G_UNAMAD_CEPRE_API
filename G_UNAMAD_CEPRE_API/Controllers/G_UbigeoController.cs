87a50b70c78bc451717e28bcf97c0f1a52466baf
Junior Alain <jrancsar@gmail.com>
2022-12-26 22:09:24 -0500

    G_Ubigeo: Creación CRUD Data y Controller peticiones HTTP

---
+++ G_UNAMAD_CEPRE_API/Controllers/G_UbigeoController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using G_UNAMAD_CEPRE_API.Data;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G_UNAMAD_CEPRE_API.Controllers
+{
+    [ApiController]
+    [Route("api/Ubigeo")]
+    public class G_UbigeoController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public G_UbigeoController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<G_UbigeoModel>>> G_UbigeoGetAll()
+        {
+            var funcion = new G_UbigeoData();
+
+            return await funcion.G_UbigeoSelectAll();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<G_UbigeoModel>> G_UbigeoGetId(string id)
+        {
+            var funcion = new G_UbigeoData();
+
+            return await funcion.G_UbigeoSelectId(id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ResponseSP>> G_UbigeoPost([FromBody]G_UbigeoModelDTO g_UbigeoModelDTO)
+        {
+            var funcion = new G_UbigeoData();
+            G_UbigeoModel g_UbigeoModel = _mapper.Map<G_UbigeoModel>(g_UbigeoModelDTO);
+
+            return await funcion.G_UbigeoInsert(g_UbigeoModel);
+        }
+        [HttpPut("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_UbigeoPut(string id, string version, [FromBody] G_UbigeoModelDTO g_UbigeoModelDTO)
+        {
+            var funcion = new G_UbigeoData();
+            G_UbigeoModel g_UbigeoModel = _mapper.Map<G_UbigeoModel>(g_UbigeoModelDTO);
+            g_UbigeoModel.IdUbigeo = id;
+            g_UbigeoModel.CVersion = version;
+
+            return await funcion.G_UbigeoUpdate(g_UbigeoModel);
+        }
+        [HttpDelete("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_UbigeoDelete(string id, string version)
+        {
+            var funcion = new G_UbigeoData();
+
+            return await funcion.G_UbigeoDelete(id, version);
+        }
+    }    
+}
---
+++ G_UNAMAD_CEPRE_API/Data/G_UbigeoData.cs
@@ -0,0 +1,159 @@
+﻿using G_UNAMAD_CEPRE_API.Connection;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace G_UNAMAD_CEPRE_API.Data
+{
+    public class G_UbigeoData : ConvertVersion
+    {
+        ConnectionBd cn = new ConnectionBd();
+        public async Task<List<G_UbigeoModel>> G_UbigeoSelectAll()
+        {
+            var lista = new List<G_UbigeoModel>();
+            using(var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using(var cmd = new SqlCommand("SP_G_UbigeoSelectAll", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    await sql.OpenAsync();
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        while(await item.ReadAsync())
+                        {
+                            var g_UbigeoModel = new G_UbigeoModel();
+                            g_UbigeoModel.IdUbigeo = (string)item["idUbigeo"];
+                            g_UbigeoModel.CDepartamento = (string)item["cDepartamento"];
+                            g_UbigeoModel.NDepartamento = (string)item["nDepartamento"];
+                            g_UbigeoModel.CProvincia = (string)item["cProvincia"];
+                            g_UbigeoModel.NProvincia = (string)item["nProvincia"];
+                            g_UbigeoModel.CDistrito = (string)item["cDistrito"];
+                            g_UbigeoModel.NDistrito = (string)item["nDistrito"];
+                            g_UbigeoModel.Activo = (int)item["activo"];
+                            g_UbigeoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_UbigeoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                            lista.Add(g_UbigeoModel);
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+        public async Task<G_UbigeoModel> G_UbigeoSelectId(string idUbigeo)
+        {
+            var g_UbigeoModel = new G_UbigeoModel();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_UbigeoSelectId", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idUbigeo", idUbigeo);
+                    await sql.OpenAsync();
+
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {                            
+                            g_UbigeoModel.IdUbigeo = (string)item["idUbigeo"];
+                            g_UbigeoModel.CDepartamento = (string)item["cDepartamento"];
+                            g_UbigeoModel.NDepartamento = (string)item["nDepartamento"];
+                            g_UbigeoModel.CProvincia = (string)item["cProvincia"];
+                            g_UbigeoModel.NProvincia = (string)item["nProvincia"];
+                            g_UbigeoModel.CDistrito = (string)item["cDistrito"];
+                            g_UbigeoModel.NDistrito = (string)item["nDistrito"];
+                            g_UbigeoModel.Activo = (int)item["activo"];
+                            g_UbigeoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_UbigeoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                        }                        
+                    }
+                }
+            }
+            return g_UbigeoModel;
+        }
+        public async Task<ResponseSP> G_UbigeoInsert(G_UbigeoModel g_UbigeoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_UbigeoInsert", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idUbigeo", g_UbigeoModel.IdUbigeo);
+                    cmd.Parameters.AddWithValue("@cDepartamento", g_UbigeoModel.CDepartamento);
+                    cmd.Parameters.AddWithValue("@nDepartamento", g_UbigeoModel.NDepartamento);
+                    cmd.Parameters.AddWithValue("@cProvincia", g_UbigeoModel.CProvincia);
+                    cmd.Parameters.AddWithValue("@nProvincia", g_UbigeoModel.NProvincia);
+                    cmd.Parameters.AddWithValue("@cDistrito", g_UbigeoModel.CDistrito);
+                    cmd.Parameters.AddWithValue("@nDistrito", g_UbigeoModel.NDistrito);
+                    cmd.Parameters.AddWithValue("@activo", g_UbigeoModel.Activo);
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+        public async Task<ResponseSP> G_UbigeoUpdate(G_UbigeoModel g_UbigeoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_UbigeoUpdate", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idUbigeo", g_UbigeoModel.IdUbigeo);
+                    cmd.Parameters.AddWithValue("@cDepartamento", g_UbigeoModel.CDepartamento);
+                    cmd.Parameters.AddWithValue("@nDepartamento", g_UbigeoModel.NDepartamento);
+                    cmd.Parameters.AddWithValue("@cProvincia", g_UbigeoModel.CProvincia);
+                    cmd.Parameters.AddWithValue("@nProvincia", g_UbigeoModel.NProvincia);
+                    cmd.Parameters.AddWithValue("@cDistrito", g_UbigeoModel.CDistrito);
+                    cmd.Parameters.AddWithValue("@nDistrito", g_UbigeoModel.NDistrito);
+                    cmd.Parameters.AddWithValue("@activo", g_UbigeoModel.Activo);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)g_UbigeoModel.CVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }            
+            return responseSP;
+        }
+        public async Task<ResponseSP>G_UbigeoDelete(string idUbigeo, string cVersion)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_UbigeoDelete", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idUbigeo", idUbigeo);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)cVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+    }    
+}
