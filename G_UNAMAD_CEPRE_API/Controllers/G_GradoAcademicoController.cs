3bd809e8cf25cd8e6f3145edeb2756dc923e4efe
Junior Alain <jrancsar@gmail.com>
2022-12-26 20:48:59 -0500

    G_GradoAcademico: Creación CRUD Data y Controller peticiones HTTP

---
+++ G_UNAMAD_CEPRE_API/Controllers/G_GradoAcademicoController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using G_UNAMAD_CEPRE_API.Data;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G_UNAMAD_CEPRE_API.Controllers
+{
+    [ApiController]
+    [Route("api/GradoAcademico")]
+    public class G_GradoAcademicoController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public G_GradoAcademicoController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<G_GradoAcademicoModel>>> G_GradoAcademicoGetAll()
+        {
+            var funcion = new G_GradoAcademicoData();
+
+            return await funcion.G_GradoAcademicoSelectAll();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<G_GradoAcademicoModel>> G_GradoAcademicoGetId(string id)
+        {
+            var funcion = new G_GradoAcademicoData();
+
+            return await funcion.G_GradoAcademicoSelectId(id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ResponseSP>> G_GradoAcademicoPost([FromBody]G_GradoAcademicoModelDTO g_GradoAcademicoModelDTO)
+        {
+            var funcion = new G_GradoAcademicoData();
+            G_GradoAcademicoModel g_GradoAcademicoModel = _mapper.Map<G_GradoAcademicoModel>(g_GradoAcademicoModelDTO);
+
+            return await funcion.G_GradoAcademicoInsert(g_GradoAcademicoModel);
+        }
+        [HttpPut("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_GradoAcademicoPut(string id, string version, [FromBody] G_GradoAcademicoModelDTO g_GradoAcademicoModelDTO)
+        {
+            var funcion = new G_GradoAcademicoData();
+            G_GradoAcademicoModel g_GradoAcademicoModel = _mapper.Map<G_GradoAcademicoModel>(g_GradoAcademicoModelDTO);
+            g_GradoAcademicoModel.IdGradoAcademico = id;
+            g_GradoAcademicoModel.CVersion = version;
+
+            return await funcion.G_GradoAcademicoUpdate(g_GradoAcademicoModel);
+        }
+        [HttpDelete("{id}/{version}")]
+        public async Task<ActionResult<ResponseSP>>G_GradoAcademicoDelete(string id, string version)
+        {
+            var funcion = new G_GradoAcademicoData();
+
+            return await funcion.G_GradoAcademicoDelete(id, version);
+        }
+    }    
+}
---
+++ G_UNAMAD_CEPRE_API/Data/G_GradoAcademicoData.cs
@@ -0,0 +1,139 @@
+﻿using G_UNAMAD_CEPRE_API.Connection;
+using G_UNAMAD_CEPRE_API.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace G_UNAMAD_CEPRE_API.Data
+{
+    public class G_GradoAcademicoData : ConvertVersion
+    {
+        ConnectionBd cn = new ConnectionBd();
+        public async Task<List<G_GradoAcademicoModel>> G_GradoAcademicoSelectAll()
+        {
+            var lista = new List<G_GradoAcademicoModel>();
+            using(var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using(var cmd = new SqlCommand("SP_G_GradoAcademicoSelectAll", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    await sql.OpenAsync();
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        while(await item.ReadAsync())
+                        {
+                            var g_GradoAcademicoModel = new G_GradoAcademicoModel();
+                            g_GradoAcademicoModel.IdGradoAcademico = (string)item["idGradoAcademico"];
+                            g_GradoAcademicoModel.NGradoAcademico = (string)item["nGradoAcademico"];
+                            g_GradoAcademicoModel.Activo = (int)item["activo"];
+                            g_GradoAcademicoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_GradoAcademicoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                            lista.Add(g_GradoAcademicoModel);
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+        public async Task<G_GradoAcademicoModel> G_GradoAcademicoSelectId(string idGradoAcademico)
+        {
+            var g_GradoAcademicoModel = new G_GradoAcademicoModel();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_GradoAcademicoSelectId", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idGradoAcademico", idGradoAcademico);
+                    await sql.OpenAsync();
+
+                    using(var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            g_GradoAcademicoModel.IdGradoAcademico = (string)item["idGradoAcademico"];
+                            g_GradoAcademicoModel.NGradoAcademico = (string)item["nGradoAcademico"];
+                            g_GradoAcademicoModel.Activo = (int)item["activo"];
+                            g_GradoAcademicoModel.FRegistroM = (DateTime)item["fRegistroM"];
+                            g_GradoAcademicoModel.CVersion = StringVersion((byte[])item["cVersion"]);
+                        }                        
+                    }
+                }
+            }
+            return g_GradoAcademicoModel;
+        }
+        public async Task<ResponseSP> G_GradoAcademicoInsert(G_GradoAcademicoModel g_GradoAcademicoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_GradoAcademicoInsert", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idGradoAcademico", g_GradoAcademicoModel.IdGradoAcademico);
+                    cmd.Parameters.AddWithValue("@nGradoAcademico", g_GradoAcademicoModel.NGradoAcademico);
+                    cmd.Parameters.AddWithValue("@activo", g_GradoAcademicoModel.Activo);
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+        public async Task<ResponseSP> G_GradoAcademicoUpdate(G_GradoAcademicoModel g_GradoAcademicoModel)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_GradoAcademicoUpdate", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idGradoAcademico", g_GradoAcademicoModel.IdGradoAcademico);
+                    cmd.Parameters.AddWithValue("@nGradoAcademico", g_GradoAcademicoModel.NGradoAcademico);
+                    cmd.Parameters.AddWithValue("@activo", g_GradoAcademicoModel.Activo);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)g_GradoAcademicoModel.CVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }            
+            return responseSP;
+        }
+        public async Task<ResponseSP>G_GradoAcademicoDelete(string idGradoAcademico, string cVersion)
+        {
+            var responseSP = new ResponseSP();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("SP_G_GradoAcademicoDelete", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idGradoAcademico", idGradoAcademico);
+                    cmd.Parameters.AddWithValue("@cVersion", ByteVersion((string)cVersion));
+                    await sql.OpenAsync();
+
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        if (await item.ReadAsync())
+                        {
+                            responseSP.TotalAffectedRecord = (int)item["totalAffectedRecord"];
+                            responseSP.MessageException = (string)item["messageException"];
+                        }
+                    }
+                }
+            }
+            return responseSP;
+        }
+    }    
+}
