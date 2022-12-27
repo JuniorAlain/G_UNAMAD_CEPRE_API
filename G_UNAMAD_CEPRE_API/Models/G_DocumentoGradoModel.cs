2fa87bef4fb23ae2ee2542431102118caa8ed581
Junior Alain <jrancsar@gmail.com>
2022-12-26 21:21:07 -0500

    G_DocumentoGrado: Creacion de clase Modelo y DTO

---
+++ G_UNAMAD_CEPRE_API/Models/G_DocumentoGradoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_DocumentoGradoModel
+    {
+        public string IdDocumentoGrado { get; set; }
+        public string NDocumentoGrado { get; set; }
+        public int Activo { get; set; }
+        public DateTime FRegistroM { get; set; }
+        public string CVersion { get; set; }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Models/G_DocumentoGradoModelDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_DocumentoGradoModelDTO
+    {
+        public string idDocumentoGrado { get; set; }
+        public string nDocumentoGrado { get; set; }
+        public int activo { get; set; }
+    }
+}
