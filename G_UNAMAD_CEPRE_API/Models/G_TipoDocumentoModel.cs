6b146f0969cac482be5e099efcbcee9a91b74c12
Junior Alain <jrancsar@gmail.com>
2022-12-26 19:37:55 -0500

    G_TipoDocumento: Creacion de clase Modelo y DTO

---
+++ G_UNAMAD_CEPRE_API/Models/G_TipoDocumentoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_TipoDocumentoModel
+    {
+        public string IdTipoDocumento { get; set; }
+        public string NTipoDocumento { get; set; }
+        public int Activo { get; set; }
+        public DateTime FRegistroM { get; set; }
+        public string CVersion { get; set; }
+    }
+}
---
+++ G_UNAMAD_CEPRE_API/Models/G_TipoDocumentoModelDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace G_UNAMAD_CEPRE_API.Models
+{
+    public class G_TipoDocumentoModelDTO
+    {
+        public string idTipoDocumento { get; set; }
+        public string nTipoDocumento { get; set; }
+        public int activo { get; set; }
+    }
+}
